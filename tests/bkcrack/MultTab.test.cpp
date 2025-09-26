94dfad9a03c8007e64218ef3ed6b03bc5d0b5657
kimci86 <kimci86@hotmail.fr>
2025-09-26 16:18:36 +0200

    Add unit tests for MultTab

---
+++ tests/bkcrack/MultTab.test.cpp
@@ -0,0 +1,36 @@
+#include <bkcrack/MultTab.hpp>
+
+#include <TestRunner.hpp>
+
+#include <algorithm>
+
+TEST("getMsbProdFiber2")
+{
+    for (auto msbprodinv = 0; msbprodinv < 256; ++msbprodinv)
+    {
+        const auto fiber = MultTab::getMsbProdFiber2(msbprodinv);
+        for (auto x = 0; x < 256; ++x)
+        {
+            const auto m               = msb(x * MultTab::multInv);
+            const auto expectedInFiber = m == msbprodinv || m == (msbprodinv + 255) % 256;
+            const auto actuallyInFiber = std::find(fiber.begin(), fiber.end(), x) != fiber.end();
+            CHECK(actuallyInFiber == expectedInFiber);
+        }
+    }
+}
+
+TEST("getMsbProdFiber3")
+{
+    for (auto msbprodinv = 0; msbprodinv < 256; ++msbprodinv)
+    {
+        const auto fiber = MultTab::getMsbProdFiber3(msbprodinv);
+        for (auto x = 0; x < 256; ++x)
+        {
+            const auto m = msb(x * MultTab::multInv);
+            const auto expectedInFiber =
+                m == msbprodinv || m == (msbprodinv + 255) % 256 || m == (msbprodinv + 1) % 256;
+            const auto actuallyInFiber = std::find(fiber.begin(), fiber.end(), x) != fiber.end();
+            CHECK(actuallyInFiber == expectedInFiber);
+        }
+    }
+}
